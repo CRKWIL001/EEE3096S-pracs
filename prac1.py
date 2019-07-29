ce26630a24b33be300ff0cce05fdd917c764d60a
William <53052697+CRKWIL001@users.noreply.github.com>
2019-07-29 21:30:35 +0200

    set up gpio pins and made an LED blink every .3seconds

--- prac1.py
+++ prac1.py
@@ -15,34 +15,35 @@ import time
 
 # Logic that you write
 def main():
-    GPIO.setwarnings(False)
+    #GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
-    
+
     output_channels = [11, 13, 15] #LEDS
     input_channels = [16, 18] #switches
-    GPIO.setup(output_channels, GPIO.IN, initial = GPIO.LOW)#setting up all output GPIOs
-    GPIO.setup(inputs_channels, GPIO.OUT, initial = GPIO.LOW)#setting up all input GPIOs
-    GPIO
-    
+    GPIO.setup(output_channels, GPIO.OUT, initial = GPIO.LOW)#setting up all output GPIOs
+    GPIO.setup(input_channels, GPIO.IN, pull_up_down = GPIO.PUD_UP)#setting up all input GPIOs
+
+    GPIO.output(11, GPIO.HIGH) #blinking an LED
+    time.sleep(0.3)
+    GPIO.output(11, GPIO.LOW)
+    time.sleep(0.3)
+
 
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-        binaryString="000"
-        print(bin(7)+"")
-        #main() #setup gpio ports
-        #while True: #loop
-            
-            
-            
-	    
+        #binaryString="000"
+        #print(bin(7)+"")
+
+        while True: #loop
+            main() #setup gpio ports
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-    except e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
+    #except e:
+     #   GPIO.cleanup()
+      #  print("Some other error occurred")
+       # print(e.message)
