9d9b525561956302aab4dd8843f2117c7822a747
William <53052697+CRKWIL001@users.noreply.github.com>
2019-07-30 12:41:42 +0200

    3-bit binary counter completed code

--- prac1.py
+++ prac1.py
@@ -12,10 +12,10 @@ Date: 29/07/2019
 # import Relevant Librares
 import RPi.GPIO as GPIO
 import time
-
 # Logic that you write
+
 def main():
-    #GPIO.setwarnings(False)
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
 
     output_channels = [11, 13, 15] #LEDS
@@ -23,22 +23,45 @@ def main():
     GPIO.setup(output_channels, GPIO.OUT, initial = GPIO.LOW)#setting up all output GPIOs
     GPIO.setup(input_channels, GPIO.IN, pull_up_down = GPIO.PUD_UP)#setting up all input GPIOs
 
-    GPIO.output(11, GPIO.HIGH) #blinking an LED
-    time.sleep(0.3)
-    GPIO.output(11, GPIO.LOW)
-    time.sleep(0.3)
+    GPIO.add_event_detect(16, GPIO.FALLING, callback = increment, bouncetime = 200)
+    GPIO.add_event_detect(18, GPIO.FALLING, callback = decrement, bouncetime = 200)
+def increment(channel):
+    global count
+    if (count<=6):
+        count = count+1
+    else:
+        count = 0
+    led()
+
+def decrement(channel):
+    global count
+    if ((count)>=1):
+        (count)-=1
+    else:
+        (count)=7
+    led()
+
+def led():
+    global count
+    binaryString = bin(count)[2:].zfill(3)
+    if GPIO.input(11)!=int(binaryString[2:]):
+        GPIO.output(11, not(GPIO.input(11)))
+    if GPIO.input(13)!=int(binaryString[1:2]):
+        GPIO.output(13, not(GPIO.input(13)))
+    if GPIO.input(15)!=int(binaryString[0:1]):
+        GPIO.output(15, not(GPIO.input(15)))
 
 
-
-# Only run the functions if 
+# Only run the functions if
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-        #binaryString="000"
-        #print(bin(7)+"")
-
-        while True: #loop
-            main() #setup gpio ports
+        count = 0
+        main()
+        while True:
+            time.sleep(.3)
+		#loop
+             #setup gpio ports
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
